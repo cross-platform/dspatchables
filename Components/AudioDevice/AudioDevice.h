946ee9e60843477661223cc5add001aaed5669d5
Marcus Tomlinson <themarcustomlinson@gmail.com>
2021-07-24 11:59:41 +0100

    Split AudioDevice into AudioIn and AudioOut

--- Components/AudioDevice/AudioDevice.h
+++ Components/AudioDevice/AudioDevice.h
@@ -86,7 +86,5 @@ private:
     std::unique_ptr<internal::AudioDevice> p;
 };
 
-EXPORT_PLUGIN( AudioDevice, true, std::vector<std::string>{ "Built-in" }, true, false )
-
 }  // namespace DSPatchables
 }  // namespace DSPatch
---
+++ Components/AudioDevice/AudioIn.cpp
@@ -0,0 +1,6 @@
+#include <AudioDevice.h>
+
+using namespace DSPatch;
+using namespace DSPatchables;
+
+EXPORT_PLUGIN( AudioDevice, false, std::vector<std::string>{ "Built-in" }, true, false )
